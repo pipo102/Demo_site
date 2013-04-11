77c32b5f66716bcf7dabd031fe6d8c4413741a8a
pipo102 <pipboy3@yahoo.com>
2013-04-12 00:04:08 +0200

    reach for the r_user sign up test

--- spec/controllers/page_controller_spec.rb
+++ spec/controllers/page_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe PageController do
-render_views
+  # render_views
+  subject {page}
+
   before(:each) do
     @main_title = "RoR SAPP |"
   end
@@ -9,30 +11,30 @@ render_views
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
-      response.should be_success
+      expect(response).to be_success
     end
 
-    it "should have appropriate title" do
-      get 'home'
-      response.should have_selector("title", :content => @main_title + " Home") 
-    end
+    # it "should have appropriate title" do
+    #   visit 'home'
+    #   expect(page).to have_title("RoR SAPP | Home") 
+    # end
   end
 
   describe "GET 'places'" do
     it "returns http success" do
       get 'places'
-      response.should be_success
+      expect(response).to be_success
     end
 
-    it "has appropriate title" do
-      get 'places'
-      response.should have_selector("title", :content => @main_title + " Places")
-    end
+    # it "has appropriate title" do
+    #   get 'places'
+    #   response.should have_selector("title", :content => @main_title + " Places")
+    # end
 
-    it "has screaming part in content" do
-      get 'places'
-      response.should have_selector("p", :content => "PLACES")
-    end
+    # it "has screaming part in content" do
+    #   get 'places'
+    #   response.should have_selector("p", :content => "PLACES")
+    # end
   end
 
   describe "GET 'contacts'" do
